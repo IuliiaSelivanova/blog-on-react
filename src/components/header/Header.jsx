0ac9fd9938b093020b410809940f14bfb8a5950b
IuliiaSelivanova <yulya.danilina.96@inbox.ru>
2025-03-20 14:23:32 +0300

    мобильная версия header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -3,12 +3,16 @@ import BackgroundImg from "../../assets/images/background.gif";
 
 const Header = () => {
   return (
-    <div className='header'>
-      <div className="headerTitles">
-        <span className="headerTitleSm">React</span>
-        <span className="headerTitleLg">Blog</span>
+    <div className="header">
+      <div className="header__title">
+        <span className="header__title--small">React</span>
+        <span className="header__title--large">Blog</span>
       </div>
-      <img className='headerImg' src={BackgroundImg} alt="background" />
+      <img
+        className="header__img"
+        src={BackgroundImg}
+        alt="background"
+      />
     </div>
   );
 };
